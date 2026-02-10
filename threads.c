75948dc95ef624252e1d1aabda4a7b45017021ab
KuysengMarakat <kuysengmarakat13@gmail.com>
2026-02-10 09:30:55 +0700

    Linux
    Linux

--- threads.c
+++ threads.c
@@ -1,67 +1,21 @@
-#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <pthread.h>
 
-DWORD Sum = 0; /* data is shared by the thread(s) */
-
-/* The thread will execute in this function */
-DWORD WINAPI Summation(LPVOID Param)
-{
-    DWORD Upper = *(DWORD*)Param;
-    
-    printf("Thread started - calculating sum from 1 to %lu...\n", Upper);
-    
-    for (DWORD i = 1; i <= Upper; i++) {
-        Sum += i;
-        Sleep(100); // Add delay in each iteration (100ms)
-    }
-    
-    printf("Thread completed calculation.\n");
-    return 0;
+void* print_message(void* arg) {
+    char* message = (char*)arg;
+    printf("%s\n", message);
+    return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        printf("Usage: %s <number>\n", argv[0]);
-        return 1;
-    }
-
-    DWORD ThreadId;
-    HANDLE ThreadHandle;
-    DWORD Param;
-
-    Param = (DWORD)atoi(argv[1]);
+int main() {
+    pthread_t thread1, thread2;
     
-    /* create the thread */
-    Sleep(5000); // Initial delay before starting the thread
-    printf("Main: Creating thread to calculate sum...\n");
-    ThreadHandle = CreateThread(
-        NULL,                   /* default security attributes */
-        0,                      /* default stack size */
-        Summation,              /* thread function */
-        &Param,                /* parameter to thread function */
-        0,                      /* default creation flags */
-        &ThreadId);            /* returns the thread identifier */
+    pthread_create(&thread1, NULL, print_message, "Hello from Thread 1");
+    pthread_create(&thread2, NULL, print_message, "Hello from Thread 2");
 
-    if (ThreadHandle == NULL) {
-        fprintf(stderr, "Error creating thread: %d\n", GetLastError());
-        return 1;
-    }
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
-
-    printf("Main: Created thread with ID: %lu\n", ThreadId);
-    printf("Main: Waiting for thread to complete...\n");
-
-    /* now wait for the thread to finish */
-    WaitForSingleObject(ThreadHandle, INFINITE);
-
-    /* close the thread handle */
-    CloseHandle(ThreadHandle);
-
-    printf("Main: Final sum = %lu\n", Sum);
-    printf("Main: Press Enter to exit...\n");
-    getchar(); // Keep console open longer
-
+    printf("Threads finished execution.\n");
     return 0;
 }
